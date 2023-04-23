529eea5b10ac1b71fee80c39f1485cd8925dca61
Abhigyan Raman <ramanabhigyan@gmail.com>
2023-04-23 19:07:32 +0000

    modification

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt-get update && apt-get install -y libsndfile1 ffmpeg
+apt-get update && apt-get install -y libsndfile1 ffmpeg
 pip install Cython
 # conda install libgcc gmp
 pip install nemo_toolkit['all']
