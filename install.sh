9ffa7e2b9b6789e87643d240435a57d700fc70a6
chaithanya <chaithanya.g@techurate.com>
2024-02-26 11:36:40 +0530

    install.sh update the version nemo_toolkit[asr]

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt-get update && apt-get install -y libsndfile1 ffmpeg
 pip install Cython
 # conda install libgcc gmp
-pip install nemo_toolkit[all]==1.21
+pip install git+https://github.com/NVIDIA/NeMo.git@r1.23.0#egg=nemo_toolkit[asr]
 
 # conda install -c conda-forge pynini==2.1.4
